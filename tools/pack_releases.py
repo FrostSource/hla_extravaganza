2af9cf2938b93aa53085bb9246a19eaa03695e8a
FrostSource <jjhfrost@gmail.com>
2021-09-09 19:51:59 +1200

    Update pack_releases.py
    Fixed relative path errors when running outside addon path.
    Added .vscode folder to scripting_environment.

--- tools/pack_releases.py
+++ tools/pack_releases.py
@@ -21,7 +21,7 @@ def get_assets_from_readme(readme_file):
                 # Get non blank asset lines
                 asset = line[1:].strip()
                 if asset != "":
-                    print("Asset found:", asset)
+                    #print("Asset found:", asset)
                     assets.append(asset)
             elif "Assets required" in line:
                 in_asset_catagory = True
@@ -35,10 +35,11 @@ def zip_path(input_path: Path, output_path: Path, filter_func: LambdaType, mode:
             for filename in filenames:
                 if filter_func(filename):
                     #print(Path( folder_name, filename ).relative_to( root ))
-                    zip_obj.write( Path( folder_name, filename ).relative_to( root ) )
+                    zip_obj.write( Path( folder_name, filename ), Path( folder_name, filename ).relative_to( root ) )
 
 if __name__ == '__main__':
     # Prefabs
+    print("Writing Prefabs...")
     output = release.joinpath("prefabs.zip")
     # assets are grabbed from readme now
     #zip_path(root.joinpath("maps/prefabs"), output, lambda fn : ".vmap" in fn and not "example.vmap" in fn)
@@ -57,16 +58,23 @@ if __name__ == '__main__':
                 zip_obj.write( asset,  asset.relative_to(root))
             else:
                 print("File Doesn't Exist (check readme):", asset)
+    print("DONE")
 
 
     # Scripting environment (helpful scripts to start a project with)
+    print("Writing Scripting Environment...")
+    lm = lambda fn : ".lua" in fn and not "__test" in fn
     output = release.joinpath("scripting_environment.zip")
-    zip_path(root.joinpath("scripts/vscripts/util"), output, lambda fn : ".lua" in fn)
+    zip_path(root.joinpath("scripts/vscripts/util"), output, lm)
+    zip_path(root.joinpath("scripts/.vscode"), output, lambda fn : fn != "", "a")
     with ZipFile( output , "a" ) as zip_obj:
-        zip_obj.write( Path( root.joinpath("scripts/vlua_globals.lua") ).relative_to( root ) )
+        zip_obj.write( root.joinpath("scripts/vlua_globals.lua"), "scripts/vlua_globals.lua" )
+    print("DONE")
 
     # FGD
+    print("Writing FGD...")
     output = release.joinpath("fgd.zip")
     with ZipFile( output , "w" ) as zip_obj:
         zip_obj.write( Path( root.joinpath("fgd/hlvr.fgd") ), "Half-Life Alyx\\game\\hlvr\\hlvr.fgd" )
         zip_obj.write( Path( root.joinpath("fgd/base.fgd") ), "Half-Life Alyx\\game\\core\\base.fgd" )
+    print("DONE")
