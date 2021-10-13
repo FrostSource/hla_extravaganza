fbefd2d78e165b3e7448f987e89dbf71261c02cf
FrostSource <jjhfrost@gmail.com>
2021-10-13 23:02:15 +1300

    Changed to colon type functions.

--- scripts/vscripts/util/storage.lua
+++ scripts/vscripts/util/storage.lua
@@ -1,5 +1,7 @@
 
 --[[
+    v1.1.0
+
     Helps with saving/loading values for persistency between game sessions.
     Values are saved into the entity running this script. If the entity
     is killed the values cannot be retrieved during that game session.
@@ -22,9 +24,9 @@
 
     Saving:
 
-    Storage.SaveVector("origin", origin)
-    Storage.SaveNumber("hp", hp)
-    Storage.SaveString("name", name)
+    Storage:SaveVector("origin", origin)
+    Storage:SaveNumber("hp", hp)
+    Storage:SaveString("name", name)
 
     Loading:
 
@@ -32,9 +34,9 @@
         -- 2 indicates a loaded game, this is typically where you load values
         -- but loading can be done at any point during the game
         if activateType == 2 then
-            origin = Storage.LoadVector("origin")
-            hp     = Storage.LoadNumber("hp")
-            name   = Storage.LoadString("name")
+            origin = Storage:LoadVector("origin")
+            hp     = Storage:LoadNumber("hp")
+            name   = Storage:LoadString("name")
         end
     end
 ]]
@@ -42,55 +44,55 @@
 Storage = Storage or {}
 
 ---Save a single number to this entity.
----@param name string Name to save as
----@param value number Variable name
-Storage.SaveNumber = function(name, value)
+---@param name string Name to save as.
+---@param value number Number to save.
+function Storage:SaveNumber(name, value)
     thisEntity:SetContextNum(name, value, 0)
 end
 
 ---Save a single string to this entity.
----@param name string Name to save as
----@param value string Variable name
-Storage.SaveString = function(name, value)
+---@param name string Name to save as.
+---@param value string String to save.
+function Storage:SaveString(name, value)
     thisEntity:SetContext(name, value, 0)
 end
 
 ---Save a Vector to this entity.
----@param name string Name to save as
----@param vector Vector Variable name
-Storage.SaveVector = function(name, vector)
-    Storage.SaveNumber(name .. ".x", vector.x)
-    Storage.SaveNumber(name .. ".y", vector.y)
-    Storage.SaveNumber(name .. ".z", vector.z)
+---@param name string Name to save as.
+---@param vector Vector Vector to save.
+function Storage:SaveVector(name, vector)
+    Storage:SaveNumber(name .. ".x", vector.x)
+    Storage:SaveNumber(name .. ".y", vector.y)
+    Storage:SaveNumber(name .. ".z", vector.z)
 end
 
 ---Save a QAngle to this entity.
----@param name string Name to save as
----@param qangle QAngle Variable name
-Storage.SaveQAngle = function(name, qangle)
-    Storage.SaveNumber(name .. ".x", qangle.x)
-    Storage.SaveNumber(name .. ".y", qangle.y)
-    Storage.SaveNumber(name .. ".z", qangle.z)
+---@param name string Name to save as.
+---@param qangle QAngle QAngle to save.
+function Storage:SaveQAngle(name, qangle)
+    Storage:SaveNumber(name .. ".x", qangle.x)
+    Storage:SaveNumber(name .. ".y", qangle.y)
+    Storage:SaveNumber(name .. ".z", qangle.z)
 end
 
 ---Load a number from this entity.
----@param name string Name the variable was saved as.
+---@param name string Name the number was saved as.
 ---@return number
-Storage.LoadNumber = function(name)
+function Storage:LoadNumber(name)
     local value = thisEntity:GetContext(name)
     if not value or type(value) ~= "number" then
-        return Warning("Number " .. name .. " could not be loaded!")
+        return print("Number " .. name .. " could not be loaded!")
     end
     return value
 end
 
 ---Load a string from this entity.
----@param name string Name the variable was saved as.
+---@param name string Name the string was saved as.
 ---@return string
-Storage.LoadString = function(name)
+function Storage:LoadString(name)
     local value = thisEntity:GetContext(name)
     if not value or type(value) ~= "string" then
-        return Warning("String " .. name .. " could not be loaded!")
+        return print("String " .. name .. " could not be loaded!")
     end
     return value
 end
@@ -98,26 +100,26 @@ end
 ---Load a Vector from this entity.
 ---@param name string Name the Vector was saved as.
 ---@return Vector
-Storage.LoadVector = function(name)
-    local x = Storage.LoadNumber(name .. ".x")
+function Storage:LoadVector(name)
+    local x = Storage:LoadNumber(name .. ".x")
     if not x then
-        return Warning("Vector " .. name .. " could not be loaded!")
+        return print("Vector " .. name .. " could not be loaded!")
     end
-    local y = Storage.LoadNumber(name .. ".y")
-    local z = Storage.LoadNumber(name .. ".z")
+    local y = Storage:LoadNumber(name .. ".y")
+    local z = Storage:LoadNumber(name .. ".z")
     return Vector(x, y, z)
 end
 
 ---Load a QAngle from this entity.
 ---@param name string Name the QAngle was saved as.
 ---@return QAngle
-Storage.LoadQAngle = function(name)
-    local x = Storage.LoadNumber(name .. ".x")
+function Storage:LoadQAngle(name)
+    local x = Storage:LoadNumber(name .. ".x")
     if not x then
-        return Warning("QAngle " .. name .. " could not be loaded!")
+        return print("QAngle " .. name .. " could not be loaded!")
     end
-    local y = Storage.LoadNumber(name .. ".y")
-    local z = Storage.LoadNumber(name .. ".z")
+    local y = Storage:LoadNumber(name .. ".y")
+    local z = Storage:LoadNumber(name .. ".z")
     return QAngle(x, y, z)
 end
 
