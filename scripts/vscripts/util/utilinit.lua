7242496a238d10a47d26122b92d156fc85777224
FrostSource <jjhfrost@gmail.com>
2021-12-28 02:36:59 +1300

    Introducing storage and player scripts.

--- scripts/vscripts/util/utilinit.lua
+++ scripts/vscripts/util/utilinit.lua
@@ -1,4 +1,14 @@
+--[[
+    v1.1.0
+
+    Initializing script for all useful utility scripts.
+    Can be set as an entity script or required from another script:
+
+        require "util.utilinit"
+]]
 --require "util.debug"
 require "util.enums"
 require "util.util"
+require "util.storage"
+require "util.player"
 require "util.weighted_random"
