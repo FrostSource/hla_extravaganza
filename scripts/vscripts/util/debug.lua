7b005ee41e3fe7e9e86cf451f95e1cdd72634846
FrostSource <jjhfrost@gmail.com>
2021-12-28 02:17:38 +1300

    Update debug.lua

--- scripts/vscripts/util/debug.lua
+++ scripts/vscripts/util/debug.lua
@@ -1,4 +1,6 @@
 --[[
+    v1.0.1
+
     Debug utility functions.
 
     Load this file at game start using the following line:
@@ -6,26 +8,15 @@
         require "util.debug"
 
 ]]
-Debug = Debug or {}
+Debug = {}
 
----Prints the given table contents to the console.
----@param t table
-function Debug:PrintTable(t)
-    local pair
-    if #t > 0 then
-        pair = ipairs(t)
-    else
-        pair = pairs(t)
-    end
-    for k,v in pair do
-        print(k, v)
-    end
-end
-
-function Debug:PrintAllEntities()
+function Debug.PrintAllEntities()
     local e = Entities:First()
+    print(string.format("\n%-40s %-40s %-40s","Classname:", "Name:", "Model Name:"))
+    print(string.format("%-40s %-40s %-40s","----------", "-----", "-----------"))
     while e ~= nil do
-        print(e:GetClassname(), e:GetName(), e:GetModelName())
+        print(string.format("%-40s %-40s %-40s", e:GetClassname(), e:GetName(), e:GetModelName()))
         e = Entities:Next(e)
     end
+    print()
 end
