e2e5137692a4f8b64a52579b9f82ebfe897560d2
FrostSource <jjhfrost@gmail.com>
2021-12-28 02:36:34 +1300

    Code cleanup and function additions.

--- scripts/vscripts/util/util.lua
+++ scripts/vscripts/util/util.lua
@@ -1,6 +1,6 @@
-
 --[[
-    This file contains utility functions to help reduce repetitive code.
+    This file contains utility functions to help reduce repetitive code
+    and add general miscellaneous functionality.
 
     Load this file at game start using the following line:
 
@@ -8,27 +8,29 @@
 
     -
 
-    Function catagories are split into nested tables within the main Util table.
+    Function catagories are split into nested tables within the main 'util' table.
 
     You can create an alias for a catagory to make referencing easier by assigning it to
     a variable within your script:
 
         local vmath = Util.Math
-        local zero = vmath.Zero()
 
 ]]
-
-Util = Util or {}
+---@diagnostic disable: lowercase-global
+util = {}
 
 ---Returns a table to be used with one of the Trace* functions.
+---
+---**Use vlua_snippets trace snippets instead.**
 ---@param startPos Vector
 ---@param endPos Vector
 ---@param ignore? CBaseEntity
 ---@param isCollideable? boolean
 ---@param min? Vector
 ---@param max? Vector
+---@deprecated
 ---@return table
-Util.TraceTable = function(startPos, endPos, ignore, isCollideable, min, max)
+function util.TraceTable(startPos, endPos, ignore, isCollideable, min, max)
     local t
     if isCollideable then
         t = {
@@ -53,7 +55,7 @@ end
 ---Converts vr_tip_attachment from a game event [1,2] into a hand id [0,1] taking into account left handedness.
 ---@param vr_tip_attachment "1"|"2"
 ---@return "0"|"1"
-Util.GetHandIdFromTip = function(vr_tip_attachment)
+function util.GetHandIdFromTip(vr_tip_attachment)
     local handId = vr_tip_attachment - 1
     if not Convars:GetBool("hlvr_left_hand_primary") then
         handId = 1 - handId
@@ -61,14 +63,50 @@ Util.GetHandIdFromTip = function(vr_tip_attachment)
     return handId
 end
 
---[[
-    Math function.
-]]
-
-Util.Math = Util.Math or {}
-
----Returns a zeroed vector.
----@return Vector
-Util.Math.Zero = function()
-    return Vector(0,0,0)
+---Estimates the nearest entity `position` with the targetname of `name`
+---(or classname `class` if the name is blank).
+---@param name string
+---@param class string
+---@param position CBaseEntity
+---@return EntityHandle
+function util.EstimateNearestEntity(name, class, position)
+    local ent
+    if name ~= "" then
+        local found_ents = Entities:FindAllByName(name)
+        -- If only one with this name exists then we can get the exact handle.
+        if #found_ents == 1 then
+            ent = found_ents[1]
+        else
+            -- If multiple exist then we need to estimate the entity that was grabbed.
+            ent = Entities:FindByNameNearest(name, position, 128)
+        end
+    else
+        -- Entity without name (hopefully doesn't happen) is found by nearest class type.
+        ent = Entities:FindByClassnameNearest(class, position, 128)
+    end
+    return ent
 end
+
+---Prints the keys/values of a table and any tested tables.
+---
+---This is different from `DeepPrintTable` in that it will not print members of entity handles.
+---@param tbl any
+---@param prefix any
+function util.PrintTable(tbl, prefix)
+    prefix = prefix or ""
+    print(prefix.."{")
+    for key, value in pairs(tbl) do
+        print( string.format( "\t%s%-32s %s", prefix, key, "= " .. (type(value) == "string" and ("\"" .. tostring(value) .. "\"") or tostring(value)) .. " ("..type(value)..")" ) )
+        if type(value)=="table" then
+            util.PrintTable(value, prefix.."\t")
+        end
+    end
+    print(prefix.."}")
+end
+
+
+--------------------
+-- Math functions --
+--------------------
+
+util.math = {}
