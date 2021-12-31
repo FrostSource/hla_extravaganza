9dc65c08ba6e81014e30aaf96136df3145e71848
FrostSource <jjhfrost@gmail.com>
2021-12-31 16:06:41 +1300

    Item outputs are fired directly now.

--- scripts/vscripts/prefabs/logic/logic_player_extension.lua
+++ scripts/vscripts/prefabs/logic/logic_player_extension.lua
@@ -4,9 +4,11 @@
 ]]
 require "util.player"
 
-------------------------
--- Getting Held Items --
-------------------------
+local items
+
+-----------------------------
+-- Getting Held Items
+-----------------------------
 
 ---Sets the item held by player's left hand as the activator in the IO chain.
 ---@param data TypeIOInvoke
@@ -29,53 +31,57 @@ local function GetHeldSecondaryHand(data)
     DoEntFire(thisEntity:GetName().."_get_held_item", "FireUser4", "", 0, Player.SecondaryHand.ItemHeld or Player.SecondaryHand or Player, thisEntity)
 end
 
---------------------------
--- Getting Items Stored --
---------------------------
+-----------------------------
+-- Getting Items Stored
+-----------------------------
 
--- ---Gets the number of energygun magazines stored in the backpack.
--- ---@param data TypeIOInvoke
--- local function GetAmmoEnergygun(data)
---     -- DoEntFire(thisEntity:GetName().."_get_ammo", "FireUser1", tostring(Player.Items.ammo.energygun), 0, data.activator, thisEntity)
---     local e = Entities:FindByName(nil, thisEntity:GetName().."_get_ammo")
---     e:FireOutput("OnUser1", data.activator, e, {}, 0)
--- end
--- ---Gets the number of shotgun magazines stored in the backpack.
--- ---@param data TypeIOInvoke
--- local function GetAmmoShotgun(data)
---     DoEntFire(thisEntity:GetName().."_get_ammo", "FireUser2", tostring(Player.Items.ammo.rapidfire), 0, data.activator, thisEntity)
--- end
--- ---Gets the number of rapidfire magazines stored in the backpack.
--- ---@param data TypeIOInvoke
--- local function GetAmmoRapidfire(data)
---     DoEntFire(thisEntity:GetName().."_get_ammo", "FireUser3", tostring(Player.Items.ammo.shotgun), 0, data.activator, thisEntity)
--- end
--- ---Gets the number of energygun magazines stored in the backpack.
--- ---@param data TypeIOInvoke
--- local function GetAmmoGenericPistol(data)
---     DoEntFire(thisEntity:GetName().."_get_ammo", "FireUser4", tostring(Player.Items.ammo.generic_pistol), 0, data.activator, thisEntity)
--- end
+---Gets the number of energygun magazines stored in the backpack.
+---@param data TypeIOInvoke
+local function GetAmmoEnergygun(data)
+    items:FireOutput("OnCase01", data.activator, thisEntity, tostring(Player.Items.ammo.energygun), 0)
+end
+---Gets the number of shotgun magazines stored in the backpack.
+---@param data TypeIOInvoke
+local function GetAmmoShotgun(data)
+    items:FireOutput("OnCase02", data.activator, thisEntity, tostring(Player.Items.ammo.shotgun), 0)
+end
+---Gets the number of rapidfire magazines stored in the backpack.
+---@param data TypeIOInvoke
+local function GetAmmoRapidfire(data)
+    items:FireOutput("OnCase03", data.activator, thisEntity, tostring(Player.Items.ammo.rapidfire), 0)
+end
+---Gets the number of energygun magazines stored in the backpack.
+---@param data TypeIOInvoke
+local function GetAmmoGenericPistol(data)
+    items:FireOutput("OnCase04", data.activator, thisEntity, tostring(Player.Items.ammo.generic_pistol), 0)
+end
 
--- ---Gets the number of frag grenades stored in the wrist pockets.
--- ---@param data TypeIOInvoke
--- local function GetItemGrenadeFrag(data)
---     DoEntFire(thisEntity:GetName().."_get_items", "FireUser1", tostring(Player.Items.grenades.frag), 0, data.activator, thisEntity)
--- end
--- ---Gets the number of xen grenades stored in the wrist pockets.
--- ---@param data TypeIOInvoke
--- local function GetItemGrenadeXen(data)
---     DoEntFire(thisEntity:GetName().."_get_items", "FireUser2", tostring(Player.Items.grenades.xen), 0, data.activator, data.caller)
--- end
--- ---Gets the number of healthpen syringes stored in the wrist pockets.
--- ---@param data TypeIOInvoke
--- local function GetItemHealthpen(data)
---     DoEntFire(thisEntity:GetName().."_get_items", "FireUser3", tostring(Player.Items.healthpen), 0, data.activator, data.caller)
--- end
+---Gets the number of frag grenades stored in the wrist pockets.
+---@param data TypeIOInvoke
+local function GetItemGrenadeFrag(data)
+    items:FireOutput("OnCase05", data.activator, thisEntity, tostring(Player.Items.grenades.frag), 0)
+end
+---Gets the number of xen grenades stored in the wrist pockets.
+---@param data TypeIOInvoke
+local function GetItemGrenadeXen(data)
+    items:FireOutput("OnCase06", data.activator, thisEntity, tostring(Player.Items.grenades.xen), 0)
+end
+---Gets the number of healthpen syringes stored in the wrist pockets.
+---@param data TypeIOInvoke
+local function GetItemHealthpen(data)
+    items:FireOutput("OnCase07", data.activator, thisEntity, tostring(Player.Items.healthpen), 0)
+end
+
+
+-----------------------------
+-- Miscellaneous Functions
+-----------------------------
 
 ---Gets the number of healthpen syringes stored in the wrist pockets.
 ---@param data TypeIOInvoke
 local function GetMoveType(data)
-    DoEntFire(thisEntity:GetName().."_get_move_type", "InValue", tostring(Player:GetMoveType()), 0, data.activator, data.caller)
+    -- DoEntFire(thisEntity:GetName().."_get_move_type", "InValue", tostring(Player:GetMoveType()), 0, data.activator, data.caller)
+    DoEntFire(thisEntity:GetName().."_get_move_type", "FireUser"..tostring(Player:GetMoveType()+1), "", 0, data.activator, thisEntity)
 end
 
 ---Gets the entity the player is looking at, otherwise activator isn't changed.
@@ -83,6 +89,47 @@ end
 local function GetLookingAt(data)
     local ent = Player:GetLookingAt()
     DoEntFire(thisEntity:GetName().."_get_looking_at", "Trigger", "", 0, ent or data.activator, data.caller)
+end
+
+---Forces the player to drop the caller entity if held.
+---@param data TypeIOInvoke
+local function DropCaller(data)
+    Player:DropByHandle(data.caller)
+end
+
+---Forces the player to drop the activator entity if held.
+---@param data TypeIOInvoke
+local function DropActivator(data)
+    Player:DropByHandle(data.activator)
+end
+
+
+
+
+-----------------------------
+-- Game Restore Fix
+-----------------------------
+
+local function ready(saveLoaded)
+    items = Entities:FindByName(nil, thisEntity:GetName().."_get_items")
+end
+
+-- Fix for script executing twice on restore.
+-- This binds to the new local ready function on second execution.
+---@diagnostic disable-next-line: undefined-field
+if thisEntity:GetPrivateScriptScope().savewasloaded then
+    thisEntity:SetContextThink("init", function() ready(true) end, 0)
+end
+
+---@param activateType "0"|"1"|"2"
+function Activate(activateType)
+    -- If game is being restored then set the script scope ready for next execution.
+    if activateType == 2 then
+        thisEntity:GetPrivateScriptScope().savewasloaded = true
+        return
+    end
+    -- Otherwise just run the ready function after "instant" delay (player will be ready).
+    thisEntity:SetContextThink("init", function() ready(false) end, 0)
 end
 
 -- Add local functions to private script scope to avoid environment pollution.
