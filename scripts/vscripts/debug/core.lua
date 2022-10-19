eeaeac6cc3d1e24e3ba3369dec7d1328a6a22444
FrostSource <jjhfrost@gmail.com>
2022-10-20 02:39:06 +1300

    Add PrintGraph function for visualizing data

--- scripts/vscripts/debug/core.lua
+++ scripts/vscripts/debug/core.lua
@@ -123,6 +123,7 @@ end
 ---@param tbl table
 ---@param prefix? string
 function Debug.PrintTable(tbl, prefix)
+    if type(tbl) ~= "table" then return end
     prefix = prefix or ""
     local visited = {tbl}
     print(prefix.."{")
@@ -174,3 +175,123 @@ function CBaseEntity:PrintCriteria()
     Debug.PrintEntityCriteria(self)
 end
 
+-- local function add_column(...)
+
+function Debug.PrintGraph(height, min_val, max_val, name_value_pairs)
+    height = height or 10
+    min_val = min_val or 0
+    max_val = max_val or 1
+    local values = name_value_pairs
+    local max_name_height = 0
+    local max_gutter = max(#tostring(min_val), #tostring(max_val))
+    for i = 1, #values, 2 do
+        max_name_height = max(max_name_height, #values[i])
+    end
+
+    ---@type string[]
+    local text_rows = {}
+    text_rows.__index = function (table, key)
+        return ""
+    end
+    setmetatable(text_rows, text_rows)
+
+    local function repeat_char(s, n)
+        local strs = {}
+        for i = 1, n do
+            strs[#strs+1] = s
+        end
+        return strs
+    end
+
+    ---Flatten any nested string arrays into one array
+    ---@param tbl (string|string[])[]
+    ---@return string[]
+    local function flatten_table(tbl)
+        local flattened = {}
+        for _, value in ipairs(tbl) do
+            if type(value) == "table" then
+                vlua.extend(flattened, flatten_table(value))
+            else
+                flattened[#flattened+1] = value
+            end
+        end
+        return flattened
+    end
+
+    ---Add a list of chars downwards
+    ---@param ... string|string[]
+    local function add_column(...)
+        local args = {...}
+        ---@type string[]
+        local strs = {}
+
+        -- Collect all arrays and strings into one string array
+        for index, arg in ipairs(args) do
+            if type(arg) == "table" then
+                vlua.extend(strs, flatten_table(arg))
+            else
+                strs[#strs+1] = arg
+            end
+        end
+
+        -- Append all strings downwards
+        for i, value in ipairs(strs) do
+            text_rows[i] = text_rows[i] .. value
+        end
+    end
+
+    ---Turns a string into an array of chars
+    ---@param s string
+    ---@return string[]
+    local function str_to_chars(s)
+        local strs = {}
+        for i = 1, #s do
+            strs[#strs+1] = s:sub(i,i)
+        end
+        return strs
+    end
+
+    -- gutter numbers
+    local gutter_format = "%"..max_gutter.."s"
+    local max_val_str = gutter_format:format(max_val)
+    local min_val_str = gutter_format:format(min_val)
+    for i = 1, max_gutter do
+        local top = tostring(max_val_str):sub(i,i)
+        if top == "" then top = " " end
+        local bot = tostring(min_val_str):sub(i,i)
+        if bot == "" then bot = " " end
+        add_column(top, repeat_char(" ", height-2), bot, repeat_char(" ", max_name_height))
+    end
+    -- Y line
+    add_column("^", repeat_char("|", height-2), " ", repeat_char(" ", max_name_height))
+    -- Space before first value
+    add_column(repeat_char(" ", height-1), "-", repeat_char(" ", max_name_height))
+    -- Values
+    local separate_names = false
+    for i = 1, #values, 2 do
+        local name,value = values[i], values[i+1]
+        -- ceil so if above lowest bound, at least show something
+        local top = math.ceil(RemapValClamped(value, min_val, max_val, 0, height-1))
+        print(top)
+        -- first half with name
+        add_column(repeat_char(" ", height-1-top), repeat_char("[", top), "-", str_to_chars(name), repeat_char(" ", max_name_height-#name))
+        -- add_column(repeat_char("e", height-1), "-", str_to_chars(name))
+        -- second half to complete graph line
+        add_column(repeat_char(" ", height-top-1), repeat_char("]", top), "-", repeat_char(" ", max_name_height))
+        -- add_column(repeat_char("g", height-1), "-", repeat_char(" ", max_name_height))
+        -- space between values
+        if separate_names and i < #values-1 then
+            add_column(repeat_char(" ", height-1), "-", repeat_char("|", max_name_height))
+        else
+            add_column(repeat_char(" ", height-1), "-", repeat_char(" ", max_name_height))
+        end
+    end
+    -- Finish X line
+    add_column(repeat_char(" ", height-1), ">", repeat_char(" ", max_name_height))
+
+    print()
+    for _, text_row in ipairs(text_rows) do
+        print(text_row)
+    end
+    print()
+end
