182c0e915a92d28a658e894c49fccec9ac80110b
FrostSource <jjhfrost@gmail.com>
2022-10-20 04:03:38 +1300

    Update scripting standards

--- scripts/vscripts/prefabs/npc/easy_bonemerge.lua
+++ scripts/vscripts/prefabs/npc/easy_bonemerge.lua
@@ -1,30 +1,45 @@
+--[[
+    v2.0.0
+    https://github.com/FrostSource/hla_extravaganza
 
----@param activateType "0"|"1"|"2"
-function Activate(activateType)
-    print("Bonemerge script activated", thisEntity:GetName())
-    ListenToGameEvent("npc_ragdoll_created", NpcRagdollCreated, thisEntity)
-end
+    Prefab specific functions for maps/prefabs/logic/logic_weighted_random/logic_weighted_random.vmap
+]]
 
-function NpcRagdollCreated(_, data)
+local listener
+
+---
+---Rebinds the bonemerged prop to the new ragdoll.
+---
+---@param data GAME_EVENT_NPC_RAGDOLL_CREATED
+local function NpcRagdollCreated(data)
+    if not IsValidEntity(thisEntity) then
+        StopListeningToGameEvent(listener)
+        return
+    end
     -- Check if the npc that died is THIS npc
-    if thisEntity:entindex() == data.npc_entindex then
-        -- Resolve npc/ragdoll index to handles
-        local npc = EntIndexToHScript(data.npc_entindex)
-        local ragdoll = EntIndexToHScript(data.ragdoll_entindex)
-        -- Checking for a child with name "bonemerge" to know which model to use
-        for _, child in ipairs(npc:GetChildren()) do
-            if vlua.find(child:GetName(), "bonemerge", 1) then
-                --print("Found bonemerge", child:GetModelName())
-                -- Spawning the new prop to merge with the new ragdoll
-                local prop = SpawnEntityFromTableSynchronous("prop_dynamic", {
-                    model = child:GetModelName(),
-                    solid = "0"
-                })
-                prop:SetParent(ragdoll, "!bonemerge")
-                ragdoll:SetRenderAlpha(0)
-            end
+    if thisEntity:entindex() ~= data.npc_entindex then
+        return
+    end
+    -- Stop listening to this game event. Without this errors may occur.
+    StopListeningToGameEvent(listener)
+    -- Resolve npc/ragdoll index to handles
+    local npc = EntIndexToHScript(data.npc_entindex)--[[@as EntityHandle]]
+    local ragdoll = EntIndexToHScript(data.ragdoll_entindex)--[[@as EntityHandle]]
+    -- Checking for a child with name "bonemerge" to know which model to use
+    for _, child in ipairs(npc:GetChildren()) do
+        if vlua.find(child:GetName(), 'bonemerger') then
+            child:SetParent(ragdoll, '!bonemerge')
+            ragdoll:SetRenderAlpha(0)
         end
-        -- Stop listening to this game event. Without this errors may occur.
-        StopListeningToAllGameEvents(thisEntity)
     end
 end
+
+---
+---Templated ents have an issue where the script runs but the entity doesn't activate.
+---If we listen outside of activations we will encounter errors.
+---
+---@param activateType 0|1|2
+function Activate(activateType)
+    listener = ListenToGameEvent('npc_ragdoll_created', NpcRagdollCreated, nil)
+end
+
