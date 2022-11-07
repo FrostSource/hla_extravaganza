be7f8a7e8b01beaa81d3a14ed5c057c6e23d3b8f
FrostSource <jjhfrost@gmail.com>
2022-11-07 20:22:29 +1300

    Add addon file functions

--- tools/lib/addon.py
+++ tools/lib/addon.py
@@ -1,5 +1,8 @@
 
 from pathlib import Path
+import os
+from fnmatch import fnmatch
+from typing import AnyStr
 
 __script_dir = Path(__file__)
 
@@ -37,3 +40,42 @@ else:
 
 content_path = content_path.resolve()
 game_path = game_path.resolve()
+
+__ignore_paths = [
+    '.git'
+]
+
+# Get all addon files
+def __get_files(start: str) -> list[Path]:
+    file_list: list[Path] = []
+    for dirpath, dirs, files in os.walk(start):
+        dirs[:] = [d for d in dirs if d not in __ignore_paths]
+        file_list.extend([Path(os.path.join(dirpath, file)) for file in files])
+    return file_list
+
+content_files = __get_files(content_path)
+game_files = __get_files(game_path)
+
+def find_files(files: list[Path|str], pattern: AnyStr, relative_to: Path|str = content_path):
+    if os.path.isdir(pattern): pattern = os.path.join(pattern, '*')
+    return [str(file) for file in files if fnmatch(file.relative_to(relative_to), pattern)]
+
+def exclude_files(files: list[Path|str], pattern: AnyStr|list[AnyStr], relative_to: Path|str = content_path) -> list[Path|str]:
+    if isinstance(pattern, str):
+        pattern = [pattern]
+    pattern = [os.path.join(p, '*') if os.path.isdir(p) else p for p in pattern]
+    return [file for file in files if not any(fnmatch(file.relative_to(relative_to), p) for p in pattern)]
+
+def find_content_files(pattern: AnyStr):
+    return find_files(content_files, pattern, relative_to=content_path)
+
+def find_game_files(pattern: AnyStr):
+    return find_files(game_files, pattern, relative_to=game_path)
+
+def exclude_content_files(pattern: AnyStr):
+    global content_files
+    content_files = exclude_files(content_files, pattern, content_path)
+
+def exclude_game_files(pattern: AnyStr):
+    global game_files
+    game_files = exclude_files(game_files, pattern, content_path)
