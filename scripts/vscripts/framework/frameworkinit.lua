72ebe1169093aacb3bec7d59f89a229a8598314f
FrostSource <jjhfrost@gmail.com>
2024-01-23 13:17:23 +1300

    Don't run if scalable init is enabled

--- scripts/vscripts/framework/frameworkinit.lua
+++ scripts/vscripts/framework/frameworkinit.lua
@@ -1,3 +1,7 @@
+-- Don't use frameworkinit if Scalable Init Support is enabled
+if not IsInToolsMode() and Convars:GetStr("default_enabled_addons_list"):find("2182586257") then
+    return
+end
 
 if IsServer() then
     local success, result = pcall(require, "core")
