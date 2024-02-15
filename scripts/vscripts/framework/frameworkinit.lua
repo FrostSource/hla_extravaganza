f81acaddc323e12c6261ffaee1fb4442420d5afe
FrostSource <jjhfrost@gmail.com>
2024-02-16 01:41:01 +1300

    Incorporate scalable init

--- scripts/vscripts/framework/frameworkinit.lua
+++ scripts/vscripts/framework/frameworkinit.lua
@@ -15,13 +15,46 @@ if IsServer() then
     end
 
     --
-    -- If Scalable Init Support is enabled, don't require these addon scripts
-    -- Instead the `mods/init/` folder should be supported.
+    -- If Scalable Init Support workshop addon is enabled, don't run this code.
+    -- Follow instructions below for init file naming standards.
     --
     -- If game is running in tools mode we will require even if Scalable Init Support is enabled
-    -- because it does not work even when displayed as enabled
+    -- because it does not work even when displayed as enabled.
     --
     if not Convars:GetStr("default_enabled_addons_list"):find("2182586257") or IsInToolsMode() then
-        -- Add requires here
+        -- This is a direct copy of Scalable Init Support gameinit.lua for compatibility.
+        -- It hasn't been updated since Mar 28, 2021 so it should be safe to use a copy without
+        -- worrying about changes.
+        -- This file allows for scalable expansion of multiple custom init files from multiple different addons to be active at once
+
+        -- Your final custom init file should exist at the path: "scripts/vscripts/mods/init/<YOUR_WORKSHOP_ID>.lua" - Update with this naming standard once your workshop addon has been initially submitted.
+        -- For functionality during development, your custom init path should be: "Half-Life Alyx/game/hlvr_addons/<YOUR_ADDON_NAME>/scripts/vscripts/mods/init/<YOUR_ADDON_NAME>.lua", and you should ensure Scalable Init Support is enabled.
+
+        local DIRECTORIES = {
+            "mods/init/",
+            ""
+        }
+
+        local addonList = Convars:GetStr("default_enabled_addons_list")--[[@as string]]
+
+        print("Searching scripts for enabled addon workshop IDs...")
+        for workshopID in addonList:gmatch("[^,]+") do
+
+            for _, DIRECTORY in pairs(DIRECTORIES) do
+
+                local path = DIRECTORY .. workshopID
+
+                xpcall(function()
+                    require(path)
+                    print("Executed " .. path .. ".lua")
+                end,
+                function(errorMessage)
+                    if not errorMessage:find(path .. "\']Failed to find") then
+                        warn("Error loading " .. path .. ":\n" .. errorMessage)
+                    end
+                end )
+            end
+
+        end
     end
 end
